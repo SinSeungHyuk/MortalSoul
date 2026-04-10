ebab4cb9b10960ac431a94ec17960c0ec57719aa
SinSeungHyuk <55730186+SinSeungHyuk@users.noreply.github.com>
2026-04-10 21:58:55 +0900

    feat: ProjectileObject 2D 투사체 클래스 추가

---
+++ Assets/02. Scripts/FieldObject/BattleObject/ProjectileObject.cs
@@ -0,0 +1,67 @@
+using MS.Battle;
+using UnityEngine;
+
+namespace MS.Field
+{
+    public class ProjectileObject : BattleObject
+    {
+        private Vector2 moveDir;
+        private float moveSpeed;
+
+
+        public void InitProjectile(Vector2 _moveDir, float _moveSpeed)
+        {
+            moveDir = _moveDir.normalized;
+            moveSpeed = _moveSpeed;
+        }
+
+        public void SetMoveSpeed(float _speed)
+        {
+            moveSpeed = _speed;
+        }
+
+        public void SetMoveDir(Vector2 _dir)
+        {
+            moveDir = _dir.normalized;
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D _other)
+        {
+            if (IsValidTarget(_other, out BattleSystemComponent _bsc))
+            {
+                for (int i = 0; i < hitCountPerAttack; i++)
+                {
+                    onHitCallback?.Invoke(this, _bsc);
+                }
+                maxAttackCount--;
+            }
+
+            if (maxAttackCount <= 0)
+                ObjectLifeState = FieldObjectLifeState.Death;
+        }
+
+        public override void OnUpdate(float _deltaTime)
+        {
+            base.OnUpdate(_deltaTime);
+
+            if (traceTarget != null && traceTarget.ObjectLifeState == FieldObjectLifeState.Live)
+            {
+                Vector2 targetPos = (Vector2)traceTarget.Position + (Vector2)targetOffset;
+                Vector2 curPos = transform.position;
+                transform.position = Vector2.MoveTowards(curPos, targetPos, moveSpeed * _deltaTime);
+
+                Vector2 dir = targetPos - curPos;
+                if (dir.sqrMagnitude > 0.001f)
+                {
+                    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.Euler(0, 0, angle);
+                }
+            }
+            else
+            {
+                transform.position += (Vector3)(moveDir * moveSpeed * _deltaTime);
+            }
+        }
+    }
+}
