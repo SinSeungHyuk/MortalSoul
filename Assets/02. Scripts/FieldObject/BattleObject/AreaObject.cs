f8e8b5d94fad5c014b253a6197b6743a053b64ab
SinSeungHyuk <55730186+SinSeungHyuk@users.noreply.github.com>
2026-04-10 21:59:54 +0900

    feat: AreaObject 2D 장판 클래스 추가

---
+++ Assets/02. Scripts/FieldObject/BattleObject/AreaObject.cs
@@ -0,0 +1,96 @@
+using MS.Battle;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MS.Field
+{
+    public class AreaObject : BattleObject
+    {
+        private List<FieldCharacter> attackTargetList = new List<FieldCharacter>();
+        private float attackInterval;
+        private float elapsedAttackTime;
+        private float delayTime;
+        private float elapsedDelayTime;
+
+
+        public void InitArea()
+        {
+            attackTargetList.Clear();
+            attackInterval = 0f;
+            elapsedAttackTime = 0f;
+            delayTime = 0f;
+            elapsedDelayTime = 0f;
+        }
+
+        public void SetDelay(float _delay)
+            => delayTime = _delay;
+        public void SetAttackInterval(float _attackInterval)
+        {
+            attackInterval = _attackInterval;
+            elapsedAttackTime = attackInterval;
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D _other)
+        {
+            if (_other.TryGetComponent(out FieldCharacter fieldChar))
+            {
+                if (((1 << _other.gameObject.layer) & targetLayer) != 0)
+                    attackTargetList.Add(fieldChar);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D _other)
+        {
+            if (_other.TryGetComponent(out FieldCharacter fieldChar))
+                attackTargetList.Remove(fieldChar);
+        }
+
+        public override void OnUpdate(float _deltaTime)
+        {
+            base.OnUpdate(_deltaTime);
+
+            if (traceTarget != null)
+            {
+                if (traceTarget.ObjectLifeState == FieldObjectLifeState.Live)
+                    transform.position = traceTarget.Position + targetOffset;
+                else
+                    ClearTraceTarget();
+            }
+
+            if (delayTime > 0f && elapsedDelayTime < delayTime)
+            {
+                elapsedDelayTime += _deltaTime;
+                return;
+            }
+            else if (elapsedDelayTime >= delayTime)
+            {
+                delayTime = 0f;
+                elapsedDelayTime = 0f;
+            }
+
+            elapsedAttackTime += _deltaTime;
+            if (elapsedAttackTime < attackInterval)
+                return;
+
+            if (maxAttackCount <= 0) return;
+            maxAttackCount--;
+
+            for (int i = attackTargetList.Count - 1; i >= 0; i--)
+            {
+                var attackTarget = attackTargetList[i];
+                if (!attackTarget || attackTarget.ObjectLifeState != FieldObjectLifeState.Live)
+                {
+                    attackTargetList.RemoveAt(i);
+                    continue;
+                }
+
+                for (int hitCount = 0; hitCount < hitCountPerAttack; hitCount++)
+                {
+                    onHitCallback?.Invoke(this, attackTarget.BSC);
+                }
+            }
+            elapsedAttackTime = 0f;
+        }
+    }
+}
