d38e9c4cd4baef7d316a50577d9226aa9634bf3e
SinSeungHyuk <55730186+SinSeungHyuk@users.noreply.github.com>
2026-04-10 22:03:24 +0900

    feat: MSImage 추가 - DOTween 기반 Show/Hide/Blink/SetColor

---
+++ Assets/02. Scripts/Core/UI/MSImage.cs
@@ -0,0 +1,57 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core
+{
+    public class MSImage : Image
+    {
+        private Tweener curTween;
+        private Color originalColor;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            originalColor = color;
+        }
+
+        public void Show(float _duration)
+        {
+            KillTween();
+            curTween = this.DOFade(1f, _duration);
+        }
+
+        public void Hide(float _duration, Action _onComplete = null)
+        {
+            KillTween();
+            curTween = this.DOFade(0f, _duration);
+            if (_onComplete != null)
+                curTween.OnComplete(() => _onComplete.Invoke());
+        }
+
+        public void DoBlink(float _speed)
+        {
+            KillTween();
+            curTween = this.DOFade(0f, _speed).SetLoops(-1, LoopType.Yoyo);
+        }
+
+        public void DoSetColor(Color _target, float _duration)
+        {
+            KillTween();
+            curTween = this.DOColor(_target, _duration);
+        }
+
+        public void StopTween()
+        {
+            KillTween();
+            color = originalColor;
+        }
+
+        private void KillTween()
+        {
+            curTween?.Kill();
+            curTween = null;
+        }
+    }
+}
